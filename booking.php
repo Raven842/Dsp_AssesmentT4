f4ac92a8a5c3a555c228dfdf39d446eabe87c87c
Raven <lavenderraven@duck.com>
2024-10-25 11:37:00 +1000

    message

---
+++ booking.php
@@ -0,0 +1,15 @@
+<?php 
+// requires function
+require "php/functions.php";
+
+var_dump($_POST);
+die();
+$checkIn = $_POST["checkIn"]; 
+$checkOut = $_POST["checkOut"];
+$mail = $_POST["mail"];
+$name = $_POST["name"];
+$cardNum = $_POST["cardNum"];
+
+// submission of data
+client_info($name, $mail, $checkIn, $checkOut, $cardNum);
+
--- php/functions.php
+++ php/functions.php
@@ -1,16 +1,13 @@
 <?php
-
-use Jlorente\CreditCards\CreditCardValidator;
-
-
+// Require to connect database
 require "database.php";
-// declares database for functions to interact with below
-
-
+// for credit card validation
+use Jlorente\CreditCards\CreditCardValidator;
 require "vendor/jlorente/php-credit-cards/src/CreditCardTypeConfig.php";
 require "vendor/jlorente/php-credit-cards/src/CreditCardTypeConfigList.php";
 require "vendor/jlorente/php-credit-cards/src/CreditCardValidator.php";
-global $db = new Database();
+// declares database for functions to interact with below
+$db = new Database();
 // get current guest ID
 function get_id() {
     $id = $db->query("select * from guest_Contact order by guest_ID desc limit 1")->fetch(PDO::FETCH_NUM);
@@ -26,14 +23,13 @@ function card_update($num) {
     $exists = false;
     // Note cannot contain sql injection if cardnum is valid thus further sanitisation not needed
     $valid = card_check($num);
-    $id = get_id();
     if ($valid == true) {
         if ($db->query("select * from payment where card_Num='$num'")->fetch(PDO::FETCH_NUM) == 0) {
            $exists = true;
         }           
     }
     if ($exists) {
-       $db->query("insert into payment (guest_ID, card_Num) values ($id, $num)");
+       $db->query("insert into payment (card_Num) value ($num)");
     }
     
 }
@@ -41,11 +37,22 @@ function clean_email($email) {
   $clean_email = filter_var($email,FILTER_SANITIZE_EMAIL);
   return $clean_email;
 }
-function clean_string($in) {
-  $out = filter_var($in, FILTER_SANITIZE_STRING);
+function clean_string($data) {
+  $out = filter_var($data, FILTER_SANITIZE_STRING);
+  return $out;
 }
-function name_update($name, $email) {
+function contact_update($name, $email) {
     $name = clean_string($name);
     $email = clean_email($email);
-    $data->query("insert into guest_Contact (name, email) values ($name, $email)");
+    $db->query("insert into guest_Contact (name, email) values ($name, $email)");
+}
+function checkin($in, $out) {
+    $room_num = rand(0, 500);
+    $db->query("insert into booking (check_In, check_Out, room_Num) values ($in, $out, $room_num)");
+}
+function client_info($name, $mail, $in, $out, $num) {
+    contact_update($name, $mail);
+    checkin($in, $out);
+    card_update($num);
+
 }
